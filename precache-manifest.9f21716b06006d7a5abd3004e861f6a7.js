f75dec3a5b565799b2092af32c098f12b04d55b8
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2019-02-07 16:34:27 +0100

    Updates

---
+++ precache-manifest.9f21716b06006d7a5abd3004e861f6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SNKFollower/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d27e2fc41b4aa9f56eeb",
+    "url": "/SNKFollower/static/js/runtime~main.d27e2fc4.js"
+  },
+  {
+    "revision": "84ab9a643fc33b299709",
+    "url": "/SNKFollower/static/js/main.84ab9a64.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/SNKFollower/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "84ab9a643fc33b299709",
+    "url": "/SNKFollower/static/css/main.20b378dd.chunk.css"
+  },
+  {
+    "revision": "58be6e47112c4a53605cf3ee303c0272",
+    "url": "/SNKFollower/index.html"
+  }
+];
